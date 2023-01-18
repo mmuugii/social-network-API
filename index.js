1ec378ed4e20e1800555ce850d4955aadc8bb272
Muugii Munkhbold <mmunkhbold94@gmail.com>
2023-01-18 15:45:19 -0800

    initial commit and file structure

---
+++ index.js
@@ -0,0 +1,21 @@
+// Module 18 Challenge - Social Media Backend API
+// 01/18/2023 Muugii M.
+
+const express = require("express");
+const db = require("./config/connection");
+const routes = require("./routes");
+
+const cwd = process.cwd();
+
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(routes);
+
+db.once("open", () => {
+  app.listen(PORT, () => {
+    console.log(`API server for ${activity} running on port ${PORT}!`);
+  });
+});
