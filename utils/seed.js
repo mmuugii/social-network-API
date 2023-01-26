da0d772328a938d999d0d9c9b645df2ade67052c
Muugii Munkhbold <mmunkhbold94@gmail.com>
2023-01-25 20:13:17 -0700

    updated seeding

--- utils/seed.js
+++ utils/seed.js
@@ -1,1 +1,18 @@
 const connection = require('../config/connection');
+const { User, Thought } = require('../models');
+const { users, thoughts } = require('./data');
+
+connection.on(`error`, err=>err);
+connection.once(`open`, async () => {
+    console.log(`You're Now Connected!`);
+    // drop existing
+    await User.deleteMany({});
+    // drop existing
+    await Thought.deleteMany({});
+    // add and await results
+    await Thought.insertMany(thoughts);
+    // add and await results
+    await User.insertMany(users);
+    console.info(`Seeding Done!`);
+    process.exit(0);
+});
