832567f612c9778c78797182744a7008def6e757
Muugii Munkhbold <mmunkhbold94@gmail.com>
2023-01-25 20:31:36 -0700

    finished seeds

--- utils/data.js
+++ utils/data.js
@@ -1,120 +1,96 @@
 // seeding data obtained and updated from mini project.
 
-const names = [
-  "Aaran",
-  "Aaren",
-  "Aarez",
-  "Aarman",
-  "Aaron",
-  "Aaron-James",
-  "Aarron",
-  "Aaryan",
-  "Aaryn",
-  "Aayan",
-  "Aazaan",
-  "Abaan",
-  "Abbas",
-  "Abdallah",
-  "Abdalroof",
-  "Abdihakim",
-  "Abdirahman",
-  "Abdisalam",
-  "Abdul",
-  "Abdul-Aziz",
-  "Abdulbasir",
-  "Abdulkadir",
-  "Abdulkarem",
-  "Smith",
-  "Jones",
-  "Coollastname",
-  "enter_name_here",
-  "Ze",
-  "Zechariah",
-  "Zeek",
-  "Zeeshan",
-  "Zeid",
-  "Zein",
-  "Zen",
-  "Zendel",
-  "Zenith",
-  "Zennon",
-  "Zeph",
-  "Zerah",
-  "Zhen",
-  "Zhi",
-  "Zhong",
-  "Zhuo",
-  "Zi",
-  "Zidane",
-  "Zijie",
-  "Zinedine",
-  "Zion",
-  "Zishan",
-  "Ziya",
-  "Ziyaan",
-  "Zohaib",
-  "Zohair",
-  "Zoubaeir",
-  "Zubair",
-  "Zubayr",
-  "Zuriel",
-  "Xander",
-  "Jared",
-  "Courtney",
-  "Gillian",
-  "Clark",
-  "Jared",
-  "Grace",
-  "Kelsey",
-  "Tamar",
-  "Alex",
-  "Mark",
-  "Tamar",
-  "Farish",
-  "Sarah",
-  "Nathaniel",
-  "Parker",
+const users = [
+    { 
+        username: "Smith",
+        email: "smithy@mail.com",
+    },
+    {
+        username: "Jones",
+        email: "jonesy@mail.com",
+    },
+    {
+        username: "Coollastname",
+        email: "coolio@mail.com",
+    },
+    {
+        username: "Zidane",
+        email: "zizou@mail.com",
+    },
+    {
+        username: "Zijie",
+        email: "zijie@mail.com",
+    },
+    {
+        username: "Zinedine",
+        email: "zizzou@mail.com"
+    },
+    {
+        username: "Zion",
+        email: "zion@mail.com",
+    },
+    {
+        username: 'Courtney',
+        email: "court@mail.com",
+    },
+    {
+        username: "Gillian",
+        email: "gill@mail.com",
+    },
+    {
+        username: "Clark",
+        email: "clarky@mail.com"
+    },
+    {
+        username: "Jared",
+        email: "jred@mail.com",
+    },
+    {
+        username: "Grace",
+        email: 'gravy@mail.com',
+    },
 ];
 
-const appDescriptions = [
-  "Decision Tracker",
-  "Find My Phone",
-  "Learn Piano",
-  "Starbase Defender",
-  "Tower Defense",
-  "Monopoly Money Manager",
-  "Movie trailers",
-  "Hello world",
-  "Stupid Social Media App",
-  "Notes",
-  "Messages",
-  "Email",
-  "Compass",
-  "Firefox",
-  "Running app",
-  "Cooking app",
-  "Poker",
-  "Deliveries",
+const getRandomUsers = () => {
+    return users[Math.floor(Math.random() * users.length-1)].username;
+};
+
+const thoughts = [
+    {
+        thoughtText: 'I cant believe its not butter',
+        username: getRandomUsers(),
+    },
+    {
+        thoughtText: 'Pat Mahomes is a Good QB',
+        username: getRandomUsers(),
+    },
+    {
+        thoughtText: 'But Joey BRRRR is better',
+        username: getRandomUsers(),
+    },
+    {
+        thoughtText: 'I wonder who will win the AFC?',
+        username: getRandomUsers(),
+    },
+    {
+        thoughtText: 'Man United sucks',
+        username: getRandomUsers(),
+    },
+    {
+        thoughtText: 'Man City sucks',
+        username: getRandomUsers(),
+    },
+    {
+        thoughtText: 'Liverpool sucks',
+        username: getRandomUsers(),
+    },
+    {
+        thoughtText: 'I hope 49ers win the Super Bowl',
+        username: getRandomUsers(),
+    },
 ];
 
 // Get a random item given an array
-const getRandomArrItem = (arr) => arr[Math.floor(Math.random() * arr.length)];
-
-// Gets a random full name
-const getRandomName = () =>
-  `${getRandomArrItem(names)} ${getRandomArrItem(names)}`;
-
-// Function to generate random assignments that we can add to student object.
-const getRandomAssignments = (int) => {
-  const results = [];
-  for (let i = 0; i < int; i++) {
-    results.push({
-      assignmentName: getRandomArrItem(appDescriptions),
-      score: Math.floor(Math.random() * (99 - 70 + 1) + 70),
-    });
-  }
-  return results;
-};
 
 // Export the functions for use in seed.js
-module.exports = { getRandomName, getRandomAssignments };
+module.exports = { users, thoughts };
